1dd214fd09e8746567ce4a3511b57c95dcb5f24a
Sai Barath <saibarathr@gmail.com>
2023-09-21 16:01:00 +0530

    added spring stater  code

---
+++ src/main/java/com/manga/harbour/mh/MhApplication.java
@@ -0,0 +1,13 @@
+package com.manga.harbour.mh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MhApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manga/harbour/mh/MhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manga.harbour.mh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
