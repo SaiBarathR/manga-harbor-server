d955a369d7db5e2692006bb2bf90291a95bf2b41
Sai Barath <saibarathr@gmail.com>
2023-09-23 18:21:40 +0530

    added image class to store a list of images urls

---
+++ src/main/java/com/manga/harbour/mh/entity/Image.java
@@ -0,0 +1,15 @@
+package com.manga.harbour.mh.entity;
+
+public class Image {
+    private String url;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+       
+}
+
