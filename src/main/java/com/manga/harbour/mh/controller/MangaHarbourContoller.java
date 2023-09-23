9a35a61df5d5ec8de5a313feb1589daf689d8209
Sai Barath <saibarathr@gmail.com>
2023-09-23 18:25:47 +0530

    added new end points for the controller to get whole manga data or by volume or by chapter

--- src/main/java/com/manga/harbour/mh/controller/MangaHarbourContoller.java
+++ src/main/java/com/manga/harbour/mh/controller/MangaHarbourContoller.java
@@ -1,23 +1,54 @@
 package com.manga.harbour.mh.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.manga.harbour.mh.service.MangaHarbourService;
+import com.manga.harbour.mh.entity.MangaVolumeDTO;
+import com.manga.harbour.mh.service.MangaService;
 
 @CrossOrigin(origins="http://localhost:3000")
 @RestController
 public class MangaHarbourContoller {
+	
 	@Autowired
-	private MangaHarbourService mangaService;
-
-	@GetMapping("/manga/random")
-	public String getManga() {
-		return mangaService.getMangaById();
+	private MangaService mangaVolumeBuiler;
+	
+	@GetMapping("/")
+	public String checkApi() {
+		return "Api is connected";
 	}
 
+	@GetMapping("/manga/{id}")
+	public List<MangaVolumeDTO> getMangaChapterList(@PathVariable String id) {
+		return mangaVolumeBuiler.getMangaChapterListById(id);
+	}
+	
+	
+	@GetMapping("/manga/download/{id}")
+	public List<MangaVolumeDTO> getManga(@PathVariable String id) {
+		return mangaVolumeBuiler.getMangaVolumesById(id, null, null);
+	}
+	
+	@GetMapping("/manga/{mangaId}/{volume}/{chapter}")
+	public List<MangaVolumeDTO>  getVolumeAndChapterDetails(@PathVariable("mangaId") String mangaId, @PathVariable("volume") String volume, @PathVariable("chapter") String chapter) {
+		return  mangaVolumeBuiler.getMangaVolumesById(mangaId,volume,chapter);
+	}
+	
+	@GetMapping("/manga/{mangaId}/volume/{volume}")
+	public List<MangaVolumeDTO> getVolumeDetails(@PathVariable("mangaId") String mangaId, @PathVariable("volume") String volume) {
+		return mangaVolumeBuiler.getMangaVolumesById(mangaId,volume,null);
+	}
+	
+	
+	@GetMapping("/manga/{mangaId}/chatper/{chapter}")
+	public List<MangaVolumeDTO>  getChapterDetails(@PathVariable("mangaId") String mangaId, @PathVariable("chapter") String chapter) {
+		return  mangaVolumeBuiler.getMangaVolumesById(mangaId,null,chapter);
+	}
+	
+	
 }
