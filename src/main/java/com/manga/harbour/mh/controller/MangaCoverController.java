303b000d117ab2a51ed540755225063f572513db
Sai Barath <saibarathr@gmail.com>
2023-09-24 16:50:31 +0530

    added controller for cover endpoints

---
+++ src/main/java/com/manga/harbour/mh/controller/MangaCoverController.java
@@ -0,0 +1,32 @@
+package com.manga.harbour.mh.controller;
+
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.manga.harbour.mh.service.MangaCoverService;
+
+@RestController
+@CrossOrigin(origins="*")
+@RequestMapping("/manga/cover")
+public class MangaCoverController {
+
+    private final MangaCoverService mangaCoverService;
+
+    public MangaCoverController(MangaCoverService mangaCoverService) {
+        this.mangaCoverService = mangaCoverService;
+    }
+
+    @GetMapping
+    public ResponseEntity<Resource> getMangaCover(@RequestParam String url) {
+        Resource imageResource = mangaCoverService.convertImageUrlToResource(url);
+        return ResponseEntity.ok()
+                .contentType(MediaType.IMAGE_JPEG) // Set the content type for JPEG images
+                .body(imageResource);
+    }
+}
