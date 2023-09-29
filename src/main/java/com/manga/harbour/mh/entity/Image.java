6752400d64304faeeabd94a70e37d91c7b14aef2
Sai Barath <saibarathdeep@outlook.com>
2023-09-29 14:34:33 +0530

    code clean up in image class

--- src/main/java/com/manga/harbour/mh/entity/Image.java
+++ src/main/java/com/manga/harbour/mh/entity/Image.java
@@ -1,15 +1,15 @@
 package com.manga.harbour.mh.entity;
 
 public class Image {
-    private String url;
+	private String url;
 
-    public String getUrl() {
-        return url;
-    }
+	public String getUrl() {
+		return url;
+	}
 
-    public void setUrl(String url) {
-        this.url = url;
-    }
-       
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
 }
 
