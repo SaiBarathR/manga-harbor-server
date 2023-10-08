a1ad23f3d00c40130f8ff66b36f7977478250408
Sai Barath <saibarathr@gmail.com>
2023-10-08 08:56:28 +0530

    removed download logic from manga service

--- src/main/java/com/manga/harbour/mh/service/MangaService.java
+++ src/main/java/com/manga/harbour/mh/service/MangaService.java
@@ -21,74 +21,70 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.manga.harbour.mh.entity.Chapter;
-import com.manga.harbour.mh.entity.MangaVolumeDTO;
+import com.manga.harbour.mh.entity.MangaVolume;
 
 import reactor.core.publisher.Mono;
 
 import com.manga.harbour.mh.entity.Image;
-
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipOutputStream;
 
 @Service
 public class MangaService {
 
     private final WebClient client;
     private final ScheduledExecutorService executorService;
-    private final List<String> volumeZipPaths = new ArrayList<>();
 
     @Autowired
     private MangaCoverService ImageService;
 
     public MangaService(WebClient.Builder webClientBuilder) {
-        this.client = webClientBuilder.baseUrl("https://api.mangadex.org").codecs(codecs -> codecs.defaultCodecs().maxInMemorySize(25000 * 1024)).build();
+        this.client = webClientBuilder
+                .baseUrl("https://api.mangadex.org")
+                .codecs(codecs -> codecs.defaultCodecs()
+                        .maxInMemorySize(25000 * 1024))
+                .build();
         this.executorService = Executors.newScheduledThreadPool(1);
     }
 
-    public List<MangaVolumeDTO> getMangaChapterListById(String id) {
+    public List<MangaVolume> getMangaChapterListById(String id) {
         String mangaData = client.get().uri("/manga/" + id + "/aggregate?translatedLanguage[0]=en").retrieve().bodyToMono(String.class).block();
         return prepareChapterList(mangaData);
     }
 
-    public List<MangaVolumeDTO> getMangaVolumesById(String id, String selectedVolume, String selectedChapter) {
+    public List<MangaVolume> getMangaVolumesById(String id, String selectedVolume, String selectedChapter) {
         String mangaData = client.get().uri("/manga/" + id + "/aggregate?translatedLanguage[0]=en").retrieve().bodyToMono(String.class).block();
         return parseAndTransformMangaData(mangaData, selectedVolume, selectedChapter);
     }
 
     public Mono<Object> getMangaDetails(String title) {
-        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl("https://api.mangadex.org").path("manga").queryParam("limit", 4).queryParam("title", title).queryParam("includes[]", "cover_art").queryParam("contentRating[]", "safe", "suggestive");
-
+        UriComponentsBuilder builder = UriComponentsBuilder
+                .fromHttpUrl("https://api.mangadex.org")
+                .path("manga")
+                .queryParam("limit", 4)
+                .queryParam("title", title)
+                .queryParam("includes[]", "cover_art")
+                .queryParam("contentRating[]", "safe", "suggestive");
         Mono<String> mangaResponse = client.get().uri(builder.build().toUriString()).retrieve().bodyToMono(String.class).onErrorResume(throwable -> Mono.empty());
-
-        return contructMangaData(mangaResponse);
+        return constructMangaData(mangaResponse);
     }
 
     public Mono<Object> getMangaInfoById(String id) {
         try {
             UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl("https://api.mangadex.org/manga/" + id).queryParam("includes[]", "manga", "cover_art", "author", "artist", "tag", "creator");
             Mono<String> mangaResponse = client.get().uri(builder.build().toUriString()).retrieve().bodyToMono(String.class);
-            System.out.println("Retrived manga details from mangadex for Id: " + id);
-            return contructMangaData(mangaResponse);
+            System.out.println("Fetched manga details from mangadex for Id: " + id);
+            return constructMangaData(mangaResponse);
         } catch (Exception e) {
             e.printStackTrace();
         }
         return null;
     }
 
-    private Mono<Object> contructMangaData(Mono<String> mangaResponse) {
+    private Mono<Object> constructMangaData(Mono<String> mangaResponse) {
         return mangaResponse.flatMap(mangaData -> {
             MultiValueMap<String, String> queryParams = new LinkedMultiValueMap<>();
             extractMangaIdsFromResponse(mangaData).forEach(id -> queryParams.add("manga[]", id));
             UriComponentsBuilder statsBuilder = UriComponentsBuilder.fromPath("statistics/manga").queryParams(queryParams);
-
             Mono<String> statsResponse = client.get().uri(statsBuilder.build().toUriString()).retrieve().bodyToMono(String.class).onErrorResume(throwable -> Mono.empty());
-
             return statsResponse.map(statsData -> {
                 try {
                     ObjectMapper objectMapper = new ObjectMapper();
@@ -111,11 +107,9 @@ public class MangaService {
                             }
                         }
                     }
-
                     Map<String, Object> data = new HashMap<>();
                     data.put("mangaData", mangaObject);
                     data.put("statsResponse", statsObject);
-
                     return data;
                 } catch (Exception e) {
                     e.printStackTrace();
@@ -126,40 +120,8 @@ public class MangaService {
 
     }
 
-    public byte[] createMangaZipFile() {
-        try (ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream(); ZipOutputStream zipOutputStream = new ZipOutputStream(byteArrayOutputStream)) {
-            for (String volumeZipPath : volumeZipPaths) {
-                File volumeZipFile = new File(volumeZipPath);
-                if (volumeZipFile.exists()) {
-                    try (FileInputStream fis = new FileInputStream(volumeZipFile)) {
-                        ZipEntry zipEntry = new ZipEntry(volumeZipFile.getName());
-                        zipOutputStream.putNextEntry(zipEntry);
-
-                        byte[] buffer = new byte[1024];
-                        int length;
-                        while ((length = fis.read(buffer)) > 0) {
-                            zipOutputStream.write(buffer, 0, length);
-                        }
-
-                        zipOutputStream.closeEntry();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                } else {
-                    System.out.println("Volume zip file not found: " + volumeZipPath);
-                }
-            }
-            zipOutputStream.finish();
-            return byteArrayOutputStream.toByteArray();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
     private List<String> extractMangaIdsFromResponse(String mangaData) {
         List<String> mangaIds = new ArrayList<>();
-
         try {
             ObjectMapper objectMapper = new ObjectMapper();
             JsonNode rootNode = objectMapper.readTree(mangaData);
@@ -174,20 +136,19 @@ public class MangaService {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
         return mangaIds;
     }
 
-    private List<MangaVolumeDTO> prepareChapterList(String mangaData) {
+    private List<MangaVolume> prepareChapterList(String mangaData) {
         ObjectMapper objectMapper = new ObjectMapper();
         try {
             Map<String, Object> jsonMap = objectMapper.readValue(mangaData, Map.class);
             Map<String, Map<String, Object>> volumesMap = (Map<String, Map<String, Object>>) jsonMap.get("volumes");
 
-            List<MangaVolumeDTO> mangaVolumes = new ArrayList<>();
+            List<MangaVolume> mangaVolumes = new ArrayList<>();
 
             for (Map.Entry<String, Map<String, Object>> volumeEntry : volumesMap.entrySet()) {
-                MangaVolumeDTO mangaVolume = new MangaVolumeDTO();
+                MangaVolume mangaVolume = new MangaVolume();
                 mangaVolume.setVolume(volumeEntry.getKey());
 
                 Map<String, Object> chaptersMap = (Map<String, Object>) volumeEntry.getValue().get("chapters");
@@ -204,7 +165,6 @@ public class MangaService {
                 mangaVolume.setChapters(chapters);
                 mangaVolumes.add(mangaVolume);
             }
-
             return mangaVolumes;
         } catch (IOException e) {
             e.printStackTrace();
@@ -212,30 +172,25 @@ public class MangaService {
         }
     }
 
-    private List<MangaVolumeDTO> parseAndTransformMangaData(String mangaData, String selectedVolume, String selectedChapter) {
-
+    private List<MangaVolume> parseAndTransformMangaData(String mangaData, String selectedVolume, String selectedChapter) {
         ObjectMapper objectMapper = new ObjectMapper();
-
         try {
             Map<String, Object> jsonMap = objectMapper.readValue(mangaData, Map.class);
             Map<String, Map<String, Object>> volumesMap = (Map<String, Map<String, Object>>) jsonMap.get("volumes");
 
-            List<MangaVolumeDTO> mangaVolumes = new ArrayList<>();
+            List<MangaVolume> mangaVolumes = new ArrayList<>();
             for (Map.Entry<String, Map<String, Object>> volumeEntry : volumesMap.entrySet()) {
                 String volumeNumber = volumeEntry.getKey();
-
                 if ("none".equals(volumeNumber)) {
                     volumeNumber = "0";
                 }
-
                 if (selectedVolume != null && !selectedVolume.equals(volumeNumber)) {
                     continue;
                 }
-
-                MangaVolumeDTO mangaVolume = new MangaVolumeDTO();
+                MangaVolume mangaVolume = new MangaVolume();
                 mangaVolume.setVolume(volumeNumber);
-                Map<String, Object> chaptersMap = (Map<String, Object>) volumeEntry.getValue().get("chapters");
 
+                Map<String, Object> chaptersMap = (Map<String, Object>) volumeEntry.getValue().get("chapters");
                 List<Chapter> chapters = new ArrayList<>();
                 for (Map.Entry<String, Object> chapterEntry : chaptersMap.entrySet()) {
                     String chapterNumber = chapterEntry.getKey();
@@ -249,7 +204,7 @@ public class MangaService {
                     List<String> imageUrls = getImageUrlsForChapter((String) chapterData.get("id"));
                     if (imageUrls.isEmpty()) {
                         for (String otherImageUrl : ((List<String>) chapterData.get("others"))) {
-                            if(imageUrls.isEmpty()) {
+                            if (imageUrls.isEmpty()) {
                                 imageUrls = getImageUrlsForChapter(otherImageUrl);
                             }
                         }
@@ -264,7 +219,6 @@ public class MangaService {
                     chapterDTO.setImages(images);
                     chapters.add(chapterDTO);
                 }
-
                 if (chapters.isEmpty()) {
                     continue;
                 }
@@ -278,128 +232,6 @@ public class MangaService {
             return Collections.emptyList();
         }
     }
-
-    public void createFolders(List<MangaVolumeDTO> mangaVolumes) {
-        File mangaFolder = new File("Manga");
-        mangaFolder.mkdirs();
-
-        for (MangaVolumeDTO mangaVolume : mangaVolumes) {
-            String volumeNumber = mangaVolume.getVolume();
-            File volumeFolder = new File(mangaFolder, "Volume " + volumeNumber);
-            volumeFolder.mkdirs();
-            List<Chapter> chapters = mangaVolume.getChapters();
-            if (chapters.isEmpty()) {
-                continue;
-            }
-            for (Chapter chapter : chapters) {
-                String chapterNumber = chapter.getChapter();
-                List<Image> images = chapter.getImages();
-                if (!images.isEmpty()) {
-                    int imageIndex = 0;
-                    File chapterFolder = new File(volumeFolder, "Chapter " + chapterNumber);
-                    chapterFolder.mkdirs();
-                    for (Image image : images) {
-                        String imageName = "Image" + (++imageIndex) + ".png";
-                        byte[] imageBytes = ImageService.retrieveImageData(image.getUrl()).block();
-                        try (FileOutputStream outputStream = new FileOutputStream(new File(chapterFolder, imageName))) {
-                            outputStream.write(imageBytes);
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                }
-                System.out.println("Saved Chapter: " + chapterNumber);
-            }
-            System.out.println("Saved volume: " + volumeNumber);
-            String volumeZipPath = mangaFolder.getPath() + File.separator + "Volume " + volumeNumber + ".zip";
-            System.out.println("Creating Zip: " + volumeNumber);
-            createZipFile(volumeFolder.getPath(), volumeZipPath);
-            volumeZipPaths.add(volumeZipPath);
-            System.out.println("Saved Zip: " + volumeNumber);
-        }
-    }
-
-    private void createZipFile(String folderPath, String zipFileName) {
-        try {
-            Path sourceFolderPath = new File(folderPath).toPath();
-            try (ZipOutputStream zipOutputStream = new ZipOutputStream(Files.newOutputStream(Path.of(zipFileName)))) {
-                Files.walk(sourceFolderPath).filter(path -> !Files.isDirectory(path)).forEach(path -> {
-                    ZipEntry zipEntry = new ZipEntry(sourceFolderPath.relativize(path).toString());
-                    try {
-                        zipOutputStream.putNextEntry(zipEntry);
-                        Files.copy(path, zipOutputStream);
-                        zipOutputStream.closeEntry();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                });
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    //	private void createMangaZipFile(File mangaFolder) {
-    //	    try {
-    //	        try (ZipOutputStream zipOutputStream = new ZipOutputStream(new FileOutputStream("manga.zip"))) {
-    //	            for (String volumeZipPath : volumeZipPaths) {
-    //	                File volumeZipFile = new File(volumeZipPath);
-    //	                if (volumeZipFile.exists()) {
-    //	                    try (FileInputStream fis = new FileInputStream(volumeZipFile)) {
-    //	                        ZipEntry zipEntry = new ZipEntry(volumeZipFile.getName());
-    //	                        zipOutputStream.putNextEntry(zipEntry);
-    //
-    //	                        byte[] buffer = new byte[1024];
-    //	                        int length;
-    //	                        while ((length = fis.read(buffer)) > 0) {
-    //	                            zipOutputStream.write(buffer, 0, length);
-    //	                        }
-    //
-    //	                        zipOutputStream.closeEntry();
-    //	                    } catch (IOException e) {
-    //	                        e.printStackTrace();
-    //	                    }
-    //	                } else {
-    //	                    System.out.println("Volume zip file not found: " + volumeZipPath);
-    //	                }
-    //	            }
-    //	        }
-    //	    } catch (IOException e) {
-    //	        e.printStackTrace();
-    //	    }
-    //	}
-
-    //	private ZipOutputStream createMangaZipFile(File mangaFolder) {
-    //		try {
-    //			try (ZipOutputStream zipOutputStream = new ZipOutputStream(new FileOutputStream("manga.zip"))) {
-    //				for (String volumeZipPath : volumeZipPaths) {
-    //					File volumeZipFile = new File(volumeZipPath);
-    //					if (volumeZipFile.exists()) {
-    //						try (FileInputStream fis = new FileInputStream(volumeZipFile)) {
-    //							ZipEntry zipEntry = new ZipEntry(volumeZipFile.getName());  // Use only the file name, not the path
-    //							zipOutputStream.putNextEntry(zipEntry);
-    //
-    //							byte[] buffer = new byte[1024];
-    //							int length;
-    //							while ((length = fis.read(buffer)) > 0) {
-    //								zipOutputStream.write(buffer, 0, length);
-    //							}
-    //
-    //							zipOutputStream.closeEntry();
-    //						} catch (IOException e) {
-    //							e.printStackTrace();
-    //						}
-    //					} else {
-    //						System.out.println("Volume zip file not found: " + volumeZipPath);
-    //					}
-    //				}
-    //				return zipOutputStream;
-    //			}
-    //		} catch (IOException e) {
-    //			e.printStackTrace();
-    //		}
-    //		return null;
-    //	}
 
     public List<String> getImageUrlsForChapter(String chapterId) {
         List<String> imageUrls = new ArrayList<>();
