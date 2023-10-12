e4f432a0e4cab79a82e6b6822dd2b9944777f955
Sai Barath <saibarathr@gmail.com>
2023-10-12 15:36:39 +0530

    created a common download endpoint which requires mangadetails json to download

--- src/main/java/com/manga/harbour/mh/controller/MangaDownloaderController.java
+++ src/main/java/com/manga/harbour/mh/controller/MangaDownloaderController.java
@@ -1,5 +1,6 @@
 package com.manga.harbour.mh.controller;
 
+import com.manga.harbour.mh.entity.MangaDownloadDetails;
 import com.manga.harbour.mh.entity.MangaVolume;
 import com.manga.harbour.mh.service.MangaDownloaderService;
 import com.manga.harbour.mh.service.MangaService;
@@ -7,11 +8,10 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.IOException;
 import java.util.List;
 
 @RestController
-@CrossOrigin(value = {"*"}, exposedHeaders = {"Content-Disposition"})
+@CrossOrigin(value = {"*"}, exposedHeaders = {"Content-Disposition", "file-size"})
 @RequestMapping("/manga/download")
 public class MangaDownloaderController {
     @Autowired
@@ -19,22 +19,44 @@ public class MangaDownloaderController {
     @Autowired
     private MangaDownloaderService mangaDownloaderService;
 
+    @PostMapping("/")
+    public void downloadManga(@RequestBody MangaDownloadDetails mangaDownloadDetails, HttpServletResponse response) {
+        mangaDownloaderService.streamZipData(mangaDownloadDetails, response);
+    }
+
     @GetMapping("/{mangaId}")
-    public void getManga(@PathVariable String mangaId, HttpServletResponse response) {
-        List<MangaVolume> mangaVolumes = mangaService.getMangaVolumesById(mangaId, null, null);
-        mangaDownloaderService.generateMangaAsZip(mangaId, mangaVolumes, "byVolume", response);
+    public MangaDownloadDetails getManga(@PathVariable String mangaId) {
+        try {
+            List<MangaVolume> mangaVolumes = mangaService.getMangaVolumesById(mangaId, null, null);
+            return mangaDownloaderService.createFileStructureForManga(mangaId, mangaVolumes, "byVolume");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     @GetMapping("{mangaId}/{volume}")
-    public void downloadVolume(@PathVariable("mangaId") String mangaId, @PathVariable("volume") String volume, HttpServletResponse response) {
-        List<MangaVolume> mangaVolumes = mangaService.getMangaVolumesById(mangaId, volume, null);
-        mangaDownloaderService.generateMangaAsZip(mangaId, mangaVolumes, "byVolume", response);
+    public MangaDownloadDetails downloadVolume(@PathVariable("mangaId") String mangaId, @PathVariable("volume") String volume) {
+        try {
+            List<MangaVolume> mangaVolumes = mangaService.getMangaVolumesById(mangaId, volume, null);
+            return mangaDownloaderService.createFileStructureForManga(mangaId, mangaVolumes, "byVolume");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     @GetMapping("{mangaId}/{volume}/{chapter}")
-    public void downloadChapter(@PathVariable("mangaId") String mangaId, @PathVariable("volume") String volume,
-                                @PathVariable("chapter") String chapter, HttpServletResponse response) {
-        List<MangaVolume> mangaVolumes = mangaService.getMangaVolumesById(mangaId, volume, chapter);
-        mangaDownloaderService.generateMangaAsZip(mangaId, mangaVolumes, "byChapter", response);
+    public MangaDownloadDetails downloadChapter(@PathVariable("mangaId") String mangaId, @PathVariable("volume") String volume, @PathVariable("chapter") String chapter) {
+        try {
+            List<MangaVolume> mangaVolumes = mangaService.getMangaVolumesById(mangaId, volume, chapter);
+            MangaDownloadDetails mangaDownloadDetails = mangaDownloaderService.createFileStructureForManga(mangaId, mangaVolumes, "byChapter");
+            System.out.println("mangaDownloadDetails" + mangaDownloadDetails.toString());
+            return mangaDownloadDetails;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+
     }
 }
