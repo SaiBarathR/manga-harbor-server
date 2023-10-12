d97a3518fff995fa239ad2fe05ceb1ffa933aa41
Sai Barath <saibarathr@gmail.com>
2023-10-13 01:39:45 +0530

    added function to update volume list

--- src/main/java/com/manga/harbour/mh/entity/MangaDownloadDetails.java
+++ src/main/java/com/manga/harbour/mh/entity/MangaDownloadDetails.java
@@ -2,7 +2,6 @@ package com.manga.harbour.mh.entity;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.nio.file.Path;
 import java.util.ArrayList;
@@ -10,17 +9,31 @@ import java.util.List;
 
 @Data
 @AllArgsConstructor
-@NoArgsConstructor
 public class MangaDownloadDetails {
-    private List<MangaVolume> mangaData;
+    private ArrayList<String> volumes;
+    private String chapters;
     private String method;
     private long folderSize;
     private Path folderPath;
     private String name;
-    List<String> zipPaths;
+    private List<String> zipPaths;
+
+    public MangaDownloadDetails() {
+        this.volumes = new ArrayList<>();
+        this.chapters = "";
+        this.method = "";
+        this.folderSize = 0;
+        this.folderPath = null;
+        this.name = "";
+        this.zipPaths = new ArrayList<>();
+    }
 
     public void updateZipPath(String newPath) {
         zipPaths.add(newPath);
+    }
+
+    public void updateVolumeList(String volume) {
+        volumes.add(volume);
     }
 
 }
