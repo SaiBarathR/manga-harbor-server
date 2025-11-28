bcca69b7dc59cb31e8865eace16b632d6d709246
Sai Barath <saibarathr@gmail.com>
2025-11-28 12:52:08 +0530

    Remove outdated CORS origins from WebConfig

--- src/main/java/com/manga/harbour/mh/configuration/WebConfig.java
+++ src/main/java/com/manga/harbour/mh/configuration/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "https://mangaharbor.netlify.app/", "https://mangaharbor.tech/","https://mangaharbor.live/")
+                .allowedOrigins("http://localhost:3000", "https://mangaharbor.netlify.app/", "https://mangaharbor.saibarath.space/")
                 .allowedMethods("*").exposedHeaders("Content-Disposition", "file-size");
     }
 
